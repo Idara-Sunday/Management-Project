fbaa588341a449ed47fc38965d8b7a5e897f9328
Idara-Sunday <ekpeneidara90@gmail.com>
2023-11-24 11:21:02 +0100

    configured my database with config service and installed typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 
@@ -8,7 +6,8 @@ import { ConfigModule } from '@nestjs/config';
   imports:[ConfigModule.forRoot({
     isGlobal:true
   })],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+  
