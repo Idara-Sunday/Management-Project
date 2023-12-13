ef50c2fd2f60c49ee35e126635a37214879344c4
Idara-Sunday <ekpeneidara90@gmail.com>
2023-12-13 23:46:03 +0100

    hot fix

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { AppContoller } from 'src/app/app.controller';
 import { AppService } from 'src/app/app.service';
+import { User } from 'src/entities/user.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Product])],
+  imports:[TypeOrmModule.forFeature([Product,User])],
   controllers: [ProductController,AppContoller],
   providers: [ProductService,AppService],
 })
