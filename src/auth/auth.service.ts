8e99a8b2f5d45586c08802612d58ca8106182579
Idara-Sunday <ekpeneidara90@gmail.com>
2024-01-15 15:23:18 +0100

    hot fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,13 +14,13 @@ import * as bcrypt from 'bcrypt';
 import { SignInDto } from '../dto/signin.dto';
 import { JwtService } from '@nestjs/jwt';
 import { Response ,Request} from 'express';
-
+ 
 @Injectable()
 export class AuthService {
-  constructor(
+  constructor( 
     @InjectRepository(User) private readonly authRepo: Repository<User>,
-    private jwtService: JwtService,
-  ) {}
+    private jwtService: JwtService, 
+  ) {} 
 
   // IMPLEMENTING SIGN UP
   async signUp(payload: signupDTO) {
