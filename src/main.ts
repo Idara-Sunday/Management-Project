75797d82daa30a59398c87c045b181cd1dcf93c3
Idara-Sunday <ekpeneidara90@gmail.com>
2024-01-24 23:11:24 +0100

    configured the documentbuilder and the swagger module in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,14 @@ async function bootstrap() {
     whitelist:true
   }))
   app.setGlobalPrefix('api/v1')
-  await app.listen(port, ()=>console.log(`server is listening on port ${port}`) 
+  const options = new DocumentBuilder()
+  .setTitle('Management System API')
+  .setDescription('Management system api')
+  .setVersion('1.0.0')
+  .build();
+  const document = SwaggerModule.createDocument(app,options);
+  SwaggerModule.setup('api/v1',app,document)
+  await app.listen(port, ()=>console.log(`server is listening on port ${port}`)
   ); 
 } 
 bootstrap();
