82e2295397733b816fcc4cd74e800f9b4dece34f
Idara-Sunday <ekpeneidara90@gmail.com>
2024-02-18 15:43:08 +0100

    modified createProfile method to return the user's profile without return the users top credentials

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -208,7 +208,10 @@ export class AuthService {
       user      
     });
 
-    return await this.profileRepo.save(userProfile)
+    // return await this.profileRepo.save(userProfile)
+    const saveProfile = await this.profileRepo.save(userProfile);
+    saveProfile.user.userReturn();
+    return saveProfile;
 
   }catch(error){
     return error
